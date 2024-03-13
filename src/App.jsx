4881cab81bd0f672a0484f9c68e154d33ff69a1a
Sangram Bishwas <info.sangram2003@gmail.com>
2024-03-13 15:08:49 +0600

    commited first time

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './Components/Header'
+import Blogs from './Components/blogs'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className='text-4xl font-bold m-8'>Knowledge Cafe</h2>
+        </div>
+    );
+};
+
+export default Header;
