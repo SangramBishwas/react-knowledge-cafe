cff36d431882afafc0f277b21c2fdc356bead8f6
Sangram Bishwas <info.sangram2003@gmail.com>
2024-03-13 23:23:20 +0600

    some file and code added

--- src/Components/blog.jsx
+++ src/Components/blog.jsx
@@ -1,25 +1,33 @@
 import PropTypes from 'prop-types';
+import { FaBookmark } from "react-icons/fa";
 const Blog = ({ blog }) => {
-    const {author_name, cover, author_img, reading_time, posted_date} = blog
+    const {author_name, title, cover, author_img, reading_time, posted_date, hashTags} = blog
     console.log(blog)
     return (
         <div className='space-y-6 m-10'>
-            <img className='h-1/2 w-4/5 rounded' src={cover} alt="" />
+            <img className='h-1/2 rounded' src={cover} alt="" />
             <div className='flex justify-between'>
-                <div className='flex gap-4 mb-4'>
+                <div className='flex gap-4 mb-4 w-full'>
                     <img src={author_img} alt="" />
-                    <div className='flex justify-between'>
+                    <div className='flex justify-between items-center w-full'> 
                         <div>
                         <h3 className='font-bold text-xl'>{author_name}</h3>
                         <p>{posted_date}</p>
                         </div>
-                        <div>
-                            <p>{reading_time} minutes</p>
+                        <div className='flex gap-3'>
+                            <p>{reading_time} minutes read</p>
+                            <button><FaBookmark /></button>   
                         </div>
                     </div>
                 </div>
                 <div></div>
             </div>
+            <h2 className='font-bold text-xl'>{title}</h2>
+            <p>
+                {
+                    hashTags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
